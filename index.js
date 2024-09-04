89dfaf2adb6461b0dd6d4d288bce10fcc7a607cb
Kristen Connors <kdconnors15@gmail.com>
2024-09-04 14:27:20 -0600

    PLEASE WORK

---
+++ index.js
@@ -0,0 +1,13 @@
+//define the message
+const message = "you have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//define each number using equations
+const a = (4+8)
+const b = (17-3)
+const c = (3+4)
+
+//perform alert with above numbers
+alert(message + " " +a+ " " +b+ " " +c+".")
+
+
+
